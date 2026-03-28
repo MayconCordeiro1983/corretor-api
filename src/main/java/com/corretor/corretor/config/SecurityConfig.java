b1589159a6d65b35d010dad6b595295a7f666efa
MayconCordeiro1983 <mayconcordeiro.1983@outlook.com>
2026-03-28 09:48:31 -0300

    Liberando CORS para frontend Vercel

--- src/main/java/com/corretor/corretor/config/SecurityConfig.java
+++ src/main/java/com/corretor/corretor/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://corretor-frontend.onrender.com"
+    "https://corretor-api-three.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
