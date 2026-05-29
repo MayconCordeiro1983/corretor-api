1cc2607ec13c71e15fda82586c9bda0d1cf3c49d
MayconCordeiro1983 <mayconcordeiro.1983@outlook.com>
2026-05-28 21:32:04 -0300

    Configura frontend para backend no Render

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,11 +1,11 @@
-const API_URL = "http://localhost:8081";
-
-function salvarToken(token) {
-  localStorage.setItem("token", token);
-}
+const API_URL = "https://corretor-api-1.onrender.com";
 
 function obterToken() {
   return localStorage.getItem("token");
+}
+
+function salvarToken(token) {
+  localStorage.setItem("token", token);
 }
 
 function removerToken() {
